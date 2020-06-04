56ce7bcb395c56ebffcbfe0787c9b1f88cbb3059
HMu0510 <HMu0510@gmail.com>
2020-06-04 12:34:03 +0900

    보스 생성 및 총알 패턴 1,2
    - 1초 간격으로 총알 발사
    - 3.3초 간격으로 회전발사(전방향)

---
+++ UnityProject1/Assets/_LMH/Scripts/Boss.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletFactory;      //bullet prefab
+    [SerializeField] private GameObject firePosition;
+    [SerializeField] private GameObject player;
+    public int bulletMax = 8;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Fire", 0, 1.0f);
+        InvokeRepeating("SpreadFire", 2, 3.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void Fire()
+    {
+            GameObject bullet = Instantiate(bulletFactory);
+            //bullet object position(start position -> player position)
+            bullet.transform.position = firePosition.transform.position;
+            Vector3 dir = player.transform.position - transform.position;
+            dir.Normalize();
+            bullet.transform.up = dir;
+        
+    }
+    private void SpreadFire()
+    {
+        for(int i = 0; i < bulletMax; i++)
+        {
+            GameObject bullet = Instantiate(bulletFactory);
+            bullet.transform.position = transform.position;
+            float angle = 360.0f / bulletMax;
+            bullet.transform.eulerAngles = new Vector3(0, 0, i * angle);
+        }
+    }
+}
