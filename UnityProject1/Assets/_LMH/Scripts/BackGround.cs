6f74e3d48b810212942d1010938f07e03aa20569
HMu0510 <HMu0510@gmail.com>
2020-06-08 13:57:56 +0900

    알파버전 모델교체
    - 백그라운드 스크롤링
    - 캐릭터 모델 교체(플레이어, 에너미, 보스)

---
+++ UnityProject1/Assets/_LMH/Scripts/BackGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    private MeshRenderer mat;
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BackgroundScroll();
+    }
+    private void BackgroundScroll()
+    {
+        Vector2 offset = mat.material.mainTextureOffset;
+        offset.Set(0, offset.y + (speed * Time.deltaTime));
+        mat.material.mainTextureOffset = offset;
+    }
+}
